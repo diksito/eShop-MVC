a6821ce476c42e1a44ad718a87af300cc44e173e
Nikolay Angelov <nikolay.angelov.angelov@gmail.com>
2015-01-30 00:27:04 +0200

    Initial source code
    This is project template MVC 4. Need redesign and remov unnecessary code
    from it.

---
+++ ShopMVC/Infrastructure/XmlReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopMVC.Infrastructure
+{
+    public class XmlReader
+    {
+        public string XmlFilePath { get; set; }
+        public XmlReader(string filePath)
+        {
+            this.XmlFilePath = filePath;
+        }
+    }
+}
