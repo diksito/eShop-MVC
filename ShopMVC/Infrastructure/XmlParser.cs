ecc33ff9e4aad4b0ffe9ccb17c53d8d6761b7d7a
Nikolay Angelov <nikolay.angelov.angelov@gmail.com>
2015-01-30 23:37:00 +0200

    Product model

--- ShopMVC/Infrastructure/XmlParser.cs
+++ ShopMVC/Infrastructure/XmlParser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShopMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,32 @@ namespace ShopMVC.Infrastructure
         public XmlParser(string filePath)
         {
             this.XmlFilePath = filePath;
+
+            // TODO: check if file exists
+            // if so throws an exception
         }
+
+        public List<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            // TODO: Start parsing here...
+
+            return products;
+        }
+        /*
+         * XmlDocument xmlDoc= new XmlDocument(); // Create an XML document object
+xmlDoc.Load("yourXMLFile.xml"); // Load the XML document from the specified file
+
+// Get elements
+XmlNodeList girlAddress = xmlDoc.GetElementsByTagName("gAddress");
+XmlNodeList girlAge = xmlDoc.GetElementsByTagName("gAge"); 
+XmlNodeList girlCellPhoneNumber = xmlDoc.GetElementsByTagName("gPhone");
+
+// Display the results
+Console.WriteLine("Address: " + girlAddress[0].InnerText);
+Console.WriteLine("Age: " + girlAge[0].InnerText);
+Console.WriteLine("Phone Number: " + girlCellPhoneNumber[0].InnerText);
+         */
     }
 }
