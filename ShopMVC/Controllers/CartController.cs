695ee0632825c5874a32c0919b0f140d409c2221
Nikolay Angelov <nikolay.angelov.angelov@gmail.com>
2015-01-31 00:33:49 +0200

    Cart controller included without any views yet

---
+++ ShopMVC/Controllers/CartController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopMVC.Controllers
+{
+    public class CartController : Controller
+    {
+        //
+        // GET: /Cart/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Cart/CheckOut
+
+        public ActionResult CheckOut()
+        {
+            return View();
+        }
+
+    }
+}
