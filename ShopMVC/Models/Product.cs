ce173320cb0100d8f91c5e60af50d46fa8dcc5a6
Nikolay Angelov <nikolay.angelov.angelov@gmail.com>
2015-01-31 00:31:17 +0200

    ProductId should be string instead of auto increment integer

--- ShopMVC/Models/Product.cs
+++ ShopMVC/Models/Product.cs
@@ -7,7 +7,7 @@ namespace ShopMVC.Models
 {
     public class Product
     {
-        public int ProductId { get; set; }
+        public string ProductId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
