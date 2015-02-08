70aaf640c10ab5201a51126391984bc3db828d5c
Nikolay Angelov <nikolay.angelov.angelov@gmail.com>
2015-02-08 23:43:37 +0200

    Display proper name for each property

--- ShopMVC/Models/Order.cs
+++ ShopMVC/Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,16 +11,21 @@ namespace ShopMVC.Models
     {
         public int OrderId { get; set; }
         [Required(ErrorMessage = "Title field is required")]
+        [DisplayName("Title")]
         public string Title { get; set; }
         [Required(ErrorMessage = "First name field is required")]
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
         [Required(ErrorMessage = "Last name field is required")]
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
         [Required(ErrorMessage = "Address field is required")]
         public string Address { get; set; }
         [Required(ErrorMessage = "House number field is required")]
+        [DisplayName("House number")]
         public string HouseNumber { get; set; }
         [Required(ErrorMessage = "Zip code field is required")]
+        [DisplayName("Zip Code")]
         public string ZipCode { get; set; }
         [Required(ErrorMessage = "Email field is required")]
         public string Email { get; set; }
