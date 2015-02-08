3d93c8c0b5ecde053c3323038ddc0c3913db2881
Nikolay Angelov <nikolay.angelov.angelov@gmail.com>
2015-02-08 23:57:32 +0200

    New js file for paging functionality on home page

---
+++ ShopMVC/Scripts/paging.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="jquery-1.9.0.min.js" />
+
+function loadProducts(page) {
+    if (page < 0) {
+        // do nothing
+    } else {
+        $("#productList").html("<h3>Loading...</h3>");
+    }
+}
