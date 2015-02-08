bdae521f37adf6cc3a47bbe13b4228c0bef6c81c
Nikolay Angelov <nikolay.angelov.angelov@gmail.com>
2015-02-08 22:26:53 +0200

    Add quantity property to product model

--- ShopMVC/Models/Product.cs
+++ ShopMVC/Models/Product.cs
@@ -11,5 +11,6 @@ namespace ShopMVC.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+        public int Quantity { get; set; }
     }
 }
