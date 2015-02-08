dab77adf51e20c7884ea18477dc13a85ac920c60
Nikolay Angelov <nikolay.angelov.angelov@gmail.com>
2015-02-08 23:12:51 +0200

    Basket and CheckOut views. Basket view is ready

--- ShopMVC/Controllers/CartController.cs
+++ ShopMVC/Controllers/CartController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using ShopMVC.DAL;
+using ShopMVC.Infrastructure;
+using ShopMVC.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +12,7 @@ namespace ShopMVC.Controllers
 {
     public class CartController : Controller
     {
+        private ShopEntities db = new ShopEntities();
         //
         // GET: /Cart/
 
@@ -17,11 +22,65 @@ namespace ShopMVC.Controllers
         }
 
         //
+        // GET: /Bassket/
+        public ActionResult Basket()
+        {
+            XmlParser parser = new XmlParser();
+
+            List<Basket> basketItems = getBasketProducts();
+            ViewBag.CartCounter = 0;
+            if (basketItems != null)
+            {
+                ViewBag.CartCounter = basketItems.Sum(b => b.Quantity);
+            }
+
+            List<Product> myBasketProducts = new List<Product>();
+
+            foreach (var item in basketItems)
+            {
+                Product product = parser.GetProduct(item.ProductId);
+                product.Quantity = basketItems.Sum(b => b.Quantity);
+                myBasketProducts.Add(product);
+            }
+
+            BasketSession basket = new BasketSession(myBasketProducts);
+
+            return View(basket);
+        }
+
+        //
         // GET: /Cart/CheckOut
 
         public ActionResult CheckOut()
         {
             return View();
+        }
+
+        //
+        // POST: /Cart/CheckOut
+
+        public ActionResult CheckOut(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+
+                List<Basket> basketItems = getBasketProducts();
+
+                try
+                {
+                    foreach (var item in basketItems) // delete already stored items
+                        db.Baskets.Remove(item);
+                }
+                catch(Exception)
+                {
+
+                }
+                return RedirectToAction("ThankYou", "Cart");
+            }
+            else
+            {
+                return View(order);
+            }
         }
 
         //
@@ -35,12 +94,78 @@ namespace ShopMVC.Controllers
         public ActionResult Add(string productId, int qty)
         {
             if(string.IsNullOrEmpty(productId))
-                return Json( new { status = false  });
+                return Json( new { status = false });
 
             if (qty < 1)
                 return Json(new { status = false });
 
-            return Json(new { status = true });
+            string visitorId = getCurrentVisitorId();
+
+            Basket existingItem = db.Baskets.Where(b => b.VisitorId == visitorId && b.ProductId == productId).FirstOrDefault();
+            if(existingItem != null)
+            {
+                existingItem.Quantity += qty;
+                try
+                {
+                    db.Entry(existingItem).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                catch(Exception)
+                {
+                    return Json(new { status = false });
+                }
+            }
+            else
+            {
+                Basket basket = new Basket
+                {
+                    BasketId = Guid.NewGuid(),
+                    VisitorId = visitorId,
+                    Quantity = qty,
+                    ProductId = productId,
+                    CreatedDate = DateTime.UtcNow
+                };
+                try
+                {
+                    db.Baskets.Add(basket);
+                    db.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    return Json(new { status = false });
+                }
+            }
+
+            List<Basket> basketItems = db.Baskets.Where(b => b.VisitorId == visitorId).ToList();
+            int countItems = qty;
+            if (basketItems != null)
+            {
+                countItems = basketItems.Sum(a => a.Quantity);
+            }
+
+            return Json(new { status = true, qty = countItems });
+        }
+
+        /// <summary>
+        /// Get current visitor session id
+        /// </summary>
+        /// <returns>GUID as string</returns>
+        private string getCurrentVisitorId()
+        {
+            if (HttpContext.Session[Constants.SESSION_VISITOR] == null)
+                HttpContext.Session[Constants.SESSION_VISITOR] = Guid.NewGuid().ToString();
+
+            string visitorId = (string)HttpContext.Session[Constants.SESSION_VISITOR];
+
+            return visitorId;
+        }
+
+        private List<Basket> getBasketProducts()
+        {
+            string visitorId = getCurrentVisitorId();
+            List<Basket> basketItems = new List<Models.Basket>();
+            basketItems = db.Baskets.Where(b => b.VisitorId == visitorId).ToList();
+            return basketItems;
         }
 
     }
