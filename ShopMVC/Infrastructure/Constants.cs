77cfab37b25d724a28b0bf53b7c01adfefec9ad9
Nikolay Angelov <nikolay.angelov.angelov@gmail.com>
2015-02-21 17:36:54 +0200

    Add articles xml file as contant path

--- ShopMVC/Infrastructure/Constants.cs
+++ ShopMVC/Infrastructure/Constants.cs
@@ -9,5 +9,6 @@ namespace ShopMVC.Infrastructure
     {
         public const int PRODUCTS_PER_PAGE = 10;
         public const string SESSION_VISITOR = "visitor";
+        public const string ARTICLES_XML_FILE = "~/App_Data/Articles.xml";
     }
 }
