e5b1b43e14e10fe50c6238d347a34baafd6373a5
Nikolay Angelov <nikolay.angelov.angelov@gmail.com>
2015-02-21 15:21:25 +0200

    Document trim method

--- ShopMVC/Infrastructure/Utility.cs
+++ ShopMVC/Infrastructure/Utility.cs
@@ -7,6 +7,12 @@ namespace ShopMVC.Infrastructure
 {
     public class Utility
     {
+        /// <summary>
+        /// Trim text to specific length
+        /// </summary>
+        /// <param name="text">text that will be trimmed</param>
+        /// <param name="length">length of the text that will be returned</param>
+        /// <returns>trimmed text</returns>
         public static string Trim(string text, int length)
         {
             if (text.Length < length)
